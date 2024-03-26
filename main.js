5380bdabd1f8d4f5efaeddb15930f5f22caf6c96
Marcus Leong <marcusleongjx@gmail.com>
2024-03-26 15:35:58 +0800

    Prompt user to make a choice

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const choiceArray = ['rock', 'paper', 'scissors'];
+/* get a random selection from the computer*/
 function getComputerChoice() {
-    let computerChoice = choiceArray[Math.floor(Math.random()*choiceArray.length)];
-    console.log(computerChoice);
+    let computerSelection = choiceArray[Math.floor(Math.random()*choiceArray.length)];
+    console.log(computerSelection);
 }
 getComputerChoice();
+/*prompt user to make a choice for the game*/
+let playerSelection = prompt("Make a choice: rock, paper, or scissors?")
