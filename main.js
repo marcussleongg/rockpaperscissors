c1135dce8d7e542a5e67c183a3d45ee47897f3b2
Marcus Leong <marcusleongjx@gmail.com>
2024-03-26 15:24:36 +0800

    Amend array and make function for computer choice
    Array threw an error because the elements were not in quotations.
    Function to get computer's choice works by getting a random decimal number
    and multiplying it by the array's length, then rounding that to a whole
    number, and selecting the array with that position number.

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-const choiceArray = [rock, paper, scissors];
+const choiceArray = ['rock', 'paper', 'scissors'];
+function getComputerChoice() {
+    let computerChoice = choiceArray[Math.floor(Math.random()*choiceArray.length)];
+    console.log(computerChoice);
+}
+getComputerChoice();
